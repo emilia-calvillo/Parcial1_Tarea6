1abb05bf121b670487f7b09f011af79692e0d10e
emilia-calvillo <100865988+emilia-calvillo@users.noreply.github.com>
2022-09-11 00:12:47 -0500

    first commit

---
+++ LinkedListIterator.java
@@ -0,0 +1,15 @@
+public class LinkedListIterator {
+    private Node node;
+
+    public LinkedListIterator(Node head) {
+        this.node=head;
+    }
+    public boolean hasNext(){
+        return node != null;
+    }
+    public String next(){
+        String data = node.getData();
+        node=node.getNext();
+        return data;
+    }
+}
