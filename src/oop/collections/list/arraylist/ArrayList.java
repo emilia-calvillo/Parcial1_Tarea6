f7f3b49ba28f2083de8b10520c45cdba66934266
emilia-calvillo <100865988+emilia-calvillo@users.noreply.github.com>
2022-10-15 22:27:33 -0500

    Commit 2

---
+++ src/oop/collections/list/arraylist/ArrayList.java
@@ -0,0 +1,93 @@
+package oop.collections.list.arraylist;
+
+public class ArrayList {
+    private int size;
+    private String[] data;
+    public ArrayList() {
+        data = new String[2];
+    }
+
+    public void addAtFront(String data) {
+        if(size == this.data.length) {
+            increase();
+        }
+
+        for(int i=size; i>0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+        this.data[0] = data;
+        this.size++;
+    }
+
+    public void addAtTail(String data) {
+        if(size == this.data.length) {
+            increase();
+        }
+        this.data[size] = data;
+        size++;
+    }
+
+    private void increase() {
+        String []newArray = new String[this.data.length * 2];
+
+        for(int i=0; i<data.length; i++) {
+            newArray[i] = data[i];
+        }
+        data = newArray;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public ArrayListIterator getIterator() {
+        return new ArrayListIterator(this);
+    }
+
+    public boolean removeIndex(int indexToRemove) {
+        if(indexToRemove<0 || indexToRemove >= size) {
+            return false;
+        }
+        for(int i=indexToRemove; i<size-1; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        data[size] = null;
+        return true;
+    }
+
+    public void removeAll() {
+        for(int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size=0;
+    }
+
+    public boolean setAt(int indexToSearch, String data) {
+        if(indexToSearch < 0 || indexToSearch >= size) {
+            return false;
+        }
+        this.data[indexToSearch] = data;
+        return true;
+    }
+
+    public String getAt(int indexToSearch) {
+        if(indexToSearch < 0 || indexToSearch >= size) {
+            return null;
+        }
+        return this.data[indexToSearch];
+    }
+
+    public void removeAllWithValue(String value) {
+        int count = 0;
+        String []newArray = new String[data.length];
+        for(int i = 0; i < size; i++) {
+            if(!data[i].equals(value)) {
+                newArray[count] = data[i];
+                count++;
+            }
+        }
+        this.data = newArray;
+        size = count;
+    }
+}
